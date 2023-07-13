9f18d2b359ae4f95a33d5256583c40ab405f0fd5
M.Lipus <mlip007@gmail.com>
2023-07-13 15:50:04 +0200

    Drobné úpravy

--- src/pages/Uvod/index.js
+++ src/pages/Uvod/index.js
@@ -27,9 +27,13 @@ export const Uvod = () => {
             </div>
             <div id="text">
                 <h1>milujeme zmrzlinu</h1>
-                <p>A tato naše vášeň nás přivedla až do italských končin, kde jsme se několik let učili od nejlepších místních mistrů vyrábět tu nejlahodnější řemeslnou zmrzlinu, která zaručeně uchvátí vaše smysly.
+                <p>A tato naše vášeň nás přivedla až do italských končin, kde jsme se 
+                několik let učili od nejlepších místních mistrů vyrábět tu nejlahodnější 
+                řemeslnou zmrzlinu, která zaručeně uchvátí vaše smysly.
                 Crème de la Crème je kvalita, čistá a dokonalá chuť.
                 A láska. Zamilujte se s námi!</p>
+
+                <a href="/e-shop"><button type="button" class="btn btn-primary">E-shop</button></a>
             </div>
     </section>
     `
