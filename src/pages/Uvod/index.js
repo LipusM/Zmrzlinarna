66b6ef4b3b02880627c1c721386c2e7792368f6f
M.Lipus <mlip007@gmail.com>
2023-07-13 13:01:28 +0200

    Rozdělaná úvodní stránka

--- src/pages/Uvod/index.js
+++ src/pages/Uvod/index.js
@@ -1,293 +1,37 @@
 const c = console.log.bind(document)
 
 import './style.scss'
+import iceCream from './img/iceCream.png'
 
 export const Uvod = () => {
 
-    const element = document.createElement("div")
+    const element = document.createElement("main")
     
     element.innerHTML = `
-    
-Naše dorty
-
-Všechny naše dorty vyrábíme ve vlastní řemeslné pekárně v Praze v Dejvicích. Stejně jako u zmrzliny jsme naprosto nekompromisní ve výběru nejlepších surovin, vše se vyrábí ručně a polotovary, práškové směsi a hotové kupované korpusy se naší řemeslné výrobně velkým obloukem vyhýbají. Naše cukrářka Mirka při své práci úročí zkušenosti z dlouholetého působení v Ambiente.
-
-Makronky - nebarvíme - to je základní princip našich makronek. Věříme v chutě surovin a nikoliv barev. Vedle vanilkových a čokoládových vyrábíme také oříškové a pistáciové makronky.
-
-Sachr - náš sachr se liší od toho originálního tím, že je měkčí a vláčnější. Čokoládou ale rozhodně nešetříme.
-
-Cheesecake - lahodná a jemná Philadelphia spolu s jemnou citronovou chutí a křupavým korpusem - to je náš cheesecake.
-
-Ořechový dort - klasickou mouku jsme nahradili zdravější pohankovou. Milovníky ořechů jistě uspokojí.
-
-Black forest - klasický a lahodný. Smetana - čokoláda - višně, a to vše zasypáno hromadou čokoládových hoblin! Víc ke štěstí netřeba.
-
-Tiramisu - geniálně vypilovaná receptura, originální piškoty Savoiardi a kvalitní mascarpone od Galbani.
-
-Makový dort
-
-Profiterol
-
-Naše dorty
-
-Všechny naše dorty vyrábíme ve vlastní řemeslné pekárně v Praze v Dejvicích. Stejně jako u zmrzliny jsme naprosto nekompromisní ve výběru nejlepších surovin, vše se vyrábí ručně a polotovary, práškové směsi a hotové kupované korpusy se naší řemeslné výrobně velkým obloukem vyhýbají. Naše cukrářka Mirka při své práci úročí zkušenosti z dlouholetého působení v Ambiente.
-
-Makronky - nebarvíme - to je základní princip našich makronek. Věříme v chutě surovin a nikoliv barev. Vedle vanilkových a čokoládových vyrábíme také oříškové a pistáciové makronky.
-
-Sachr - náš sachr se liší od toho originálního tím, že je měkčí a vláčnější. Čokoládou ale rozhodně nešetříme.
-
-Cheesecake - lahodná a jemná Philadelphia spolu s jemnou citronovou chutí a křupavým korpusem - to je náš cheesecake.
-
-Ořechový dort - klasickou mouku jsme nahradili zdravější pohankovou. Milovníky ořechů jistě uspokojí.
-
-Black forest - klasický a lahodný. Smetana - čokoláda - višně, a to vše zasypáno hromadou čokoládových hoblin! Víc ke štěstí netřeba.
-
-Tiramisu - geniálně vypilovaná receptura, originální piškoty Savoiardi a kvalitní mascarpone od Galbani.
-
-Makový dort
-
-Profiterol
-
-Naše dorty
-
-Všechny naše dorty vyrábíme ve vlastní řemeslné pekárně v Praze v Dejvicích. Stejně jako u zmrzliny jsme naprosto nekompromisní ve výběru nejlepších surovin, vše se vyrábí ručně a polotovary, práškové směsi a hotové kupované korpusy se naší řemeslné výrobně velkým obloukem vyhýbají. Naše cukrářka Mirka při své práci úročí zkušenosti z dlouholetého působení v Ambiente.
-
-Makronky - nebarvíme - to je základní princip našich makronek. Věříme v chutě surovin a nikoliv barev. Vedle vanilkových a čokoládových vyrábíme také oříškové a pistáciové makronky.
-
-Sachr - náš sachr se liší od toho originálního tím, že je měkčí a vláčnější. Čokoládou ale rozhodně nešetříme.
-
-Cheesecake - lahodná a jemná Philadelphia spolu s jemnou citronovou chutí a křupavým korpusem - to je náš cheesecake.
-
-Ořechový dort - klasickou mouku jsme nahradili zdravější pohankovou. Milovníky ořechů jistě uspokojí.
-
-Black forest - klasický a lahodný. Smetana - čokoláda - višně, a to vše zasypáno hromadou čokoládových hoblin! Víc ke štěstí netřeba.
-
-Tiramisu - geniálně vypilovaná receptura, originální piškoty Savoiardi a kvalitní mascarpone od Galbani.
-
-Makový dort
-
-Profiterol
-
-Naše dorty
-
-Všechny naše dorty vyrábíme ve vlastní řemeslné pekárně v Praze v Dejvicích. Stejně jako u zmrzliny jsme naprosto nekompromisní ve výběru nejlepších surovin, vše se vyrábí ručně a polotovary, práškové směsi a hotové kupované korpusy se naší řemeslné výrobně velkým obloukem vyhýbají. Naše cukrářka Mirka při své práci úročí zkušenosti z dlouholetého působení v Ambiente.
-
-Makronky - nebarvíme - to je základní princip našich makronek. Věříme v chutě surovin a nikoliv barev. Vedle vanilkových a čokoládových vyrábíme také oříškové a pistáciové makronky.
-
-Sachr - náš sachr se liší od toho originálního tím, že je měkčí a vláčnější. Čokoládou ale rozhodně nešetříme.
-
-Cheesecake - lahodná a jemná Philadelphia spolu s jemnou citronovou chutí a křupavým korpusem - to je náš cheesecake.
-
-Ořechový dort - klasickou mouku jsme nahradili zdravější pohankovou. Milovníky ořechů jistě uspokojí.
-
-Black forest - klasický a lahodný. Smetana - čokoláda - višně, a to vše zasypáno hromadou čokoládových hoblin! Víc ke štěstí netřeba.
-
-Tiramisu - geniálně vypilovaná receptura, originální piškoty Savoiardi a kvalitní mascarpone od Galbani.
-
-Makový dort
-
-Profiterol
-
-Naše dorty
-
-Všechny naše dorty vyrábíme ve vlastní řemeslné pekárně v Praze v Dejvicích. Stejně jako u zmrzliny jsme naprosto nekompromisní ve výběru nejlepších surovin, vše se vyrábí ručně a polotovary, práškové směsi a hotové kupované korpusy se naší řemeslné výrobně velkým obloukem vyhýbají. Naše cukrářka Mirka při své práci úročí zkušenosti z dlouholetého působení v Ambiente.
-
-Makronky - nebarvíme - to je základní princip našich makronek. Věříme v chutě surovin a nikoliv barev. Vedle vanilkových a čokoládových vyrábíme také oříškové a pistáciové makronky.
-
-Sachr - náš sachr se liší od toho originálního tím, že je měkčí a vláčnější. Čokoládou ale rozhodně nešetříme.
-
-Cheesecake - lahodná a jemná Philadelphia spolu s jemnou citronovou chutí a křupavým korpusem - to je náš cheesecake.
-
-Ořechový dort - klasickou mouku jsme nahradili zdravější pohankovou. Milovníky ořechů jistě uspokojí.
-
-Black forest - klasický a lahodný. Smetana - čokoláda - višně, a to vše zasypáno hromadou čokoládových hoblin! Víc ke štěstí netřeba.
-
-Tiramisu - geniálně vypilovaná receptura, originální piškoty Savoiardi a kvalitní mascarpone od Galbani.
-
-Makový dort
-
-Profiterol
-
-Naše dorty
-
-Všechny naše dorty vyrábíme ve vlastní řemeslné pekárně v Praze v Dejvicích. Stejně jako u zmrzliny jsme naprosto nekompromisní ve výběru nejlepších surovin, vše se vyrábí ručně a polotovary, práškové směsi a hotové kupované korpusy se naší řemeslné výrobně velkým obloukem vyhýbají. Naše cukrářka Mirka při své práci úročí zkušenosti z dlouholetého působení v Ambiente.
-
-Makronky - nebarvíme - to je základní princip našich makronek. Věříme v chutě surovin a nikoliv barev. Vedle vanilkových a čokoládových vyrábíme také oříškové a pistáciové makronky.
-
-Sachr - náš sachr se liší od toho originálního tím, že je měkčí a vláčnější. Čokoládou ale rozhodně nešetříme.
-
-Cheesecake - lahodná a jemná Philadelphia spolu s jemnou citronovou chutí a křupavým korpusem - to je náš cheesecake.
-
-Ořechový dort - klasickou mouku jsme nahradili zdravější pohankovou. Milovníky ořechů jistě uspokojí.
-
-Black forest - klasický a lahodný. Smetana - čokoláda - višně, a to vše zasypáno hromadou čokoládových hoblin! Víc ke štěstí netřeba.
-
-Tiramisu - geniálně vypilovaná receptura, originální piškoty Savoiardi a kvalitní mascarpone od Galbani.
-
-Makový dort
-
-Profiterol
-
-Naše dorty
-
-Všechny naše dorty vyrábíme ve vlastní řemeslné pekárně v Praze v Dejvicích. Stejně jako u zmrzliny jsme naprosto nekompromisní ve výběru nejlepších surovin, vše se vyrábí ručně a polotovary, práškové směsi a hotové kupované korpusy se naší řemeslné výrobně velkým obloukem vyhýbají. Naše cukrářka Mirka při své práci úročí zkušenosti z dlouholetého působení v Ambiente.
-
-Makronky - nebarvíme - to je základní princip našich makronek. Věříme v chutě surovin a nikoliv barev. Vedle vanilkových a čokoládových vyrábíme také oříškové a pistáciové makronky.
-
-Sachr - náš sachr se liší od toho originálního tím, že je měkčí a vláčnější. Čokoládou ale rozhodně nešetříme.
-
-Cheesecake - lahodná a jemná Philadelphia spolu s jemnou citronovou chutí a křupavým korpusem - to je náš cheesecake.
-
-Ořechový dort - klasickou mouku jsme nahradili zdravější pohankovou. Milovníky ořechů jistě uspokojí.
-
-Black forest - klasický a lahodný. Smetana - čokoláda - višně, a to vše zasypáno hromadou čokoládových hoblin! Víc ke štěstí netřeba.
-
-Tiramisu - geniálně vypilovaná receptura, originální piškoty Savoiardi a kvalitní mascarpone od Galbani.
-
-Makový dort
-
-Profiterol
-
-Naše dorty
-
-Všechny naše dorty vyrábíme ve vlastní řemeslné pekárně v Praze v Dejvicích. Stejně jako u zmrzliny jsme naprosto nekompromisní ve výběru nejlepších surovin, vše se vyrábí ručně a polotovary, práškové směsi a hotové kupované korpusy se naší řemeslné výrobně velkým obloukem vyhýbají. Naše cukrářka Mirka při své práci úročí zkušenosti z dlouholetého působení v Ambiente.
-
-Makronky - nebarvíme - to je základní princip našich makronek. Věříme v chutě surovin a nikoliv barev. Vedle vanilkových a čokoládových vyrábíme také oříškové a pistáciové makronky.
-
-Sachr - náš sachr se liší od toho originálního tím, že je měkčí a vláčnější. Čokoládou ale rozhodně nešetříme.
-
-Cheesecake - lahodná a jemná Philadelphia spolu s jemnou citronovou chutí a křupavým korpusem - to je náš cheesecake.
-
-Ořechový dort - klasickou mouku jsme nahradili zdravější pohankovou. Milovníky ořechů jistě uspokojí.
-
-Black forest - klasický a lahodný. Smetana - čokoláda - višně, a to vše zasypáno hromadou čokoládových hoblin! Víc ke štěstí netřeba.
-
-Tiramisu - geniálně vypilovaná receptura, originální piškoty Savoiardi a kvalitní mascarpone od Galbani.
-
-Makový dort
-
-Profiterol
-
-Naše dorty
-
-Všechny naše dorty vyrábíme ve vlastní řemeslné pekárně v Praze v Dejvicích. Stejně jako u zmrzliny jsme naprosto nekompromisní ve výběru nejlepších surovin, vše se vyrábí ručně a polotovary, práškové směsi a hotové kupované korpusy se naší řemeslné výrobně velkým obloukem vyhýbají. Naše cukrářka Mirka při své práci úročí zkušenosti z dlouholetého působení v Ambiente.
-
-Makronky - nebarvíme - to je základní princip našich makronek. Věříme v chutě surovin a nikoliv barev. Vedle vanilkových a čokoládových vyrábíme také oříškové a pistáciové makronky.
-
-Sachr - náš sachr se liší od toho originálního tím, že je měkčí a vláčnější. Čokoládou ale rozhodně nešetříme.
-
-Cheesecake - lahodná a jemná Philadelphia spolu s jemnou citronovou chutí a křupavým korpusem - to je náš cheesecake.
-
-Ořechový dort - klasickou mouku jsme nahradili zdravější pohankovou. Milovníky ořechů jistě uspokojí.
-
-Black forest - klasický a lahodný. Smetana - čokoláda - višně, a to vše zasypáno hromadou čokoládových hoblin! Víc ke štěstí netřeba.
-
-Tiramisu - geniálně vypilovaná receptura, originální piškoty Savoiardi a kvalitní mascarpone od Galbani.
-
-Makový dort
-
-Profiterol
-
-Naše dorty
-
-Všechny naše dorty vyrábíme ve vlastní řemeslné pekárně v Praze v Dejvicích. Stejně jako u zmrzliny jsme naprosto nekompromisní ve výběru nejlepších surovin, vše se vyrábí ručně a polotovary, práškové směsi a hotové kupované korpusy se naší řemeslné výrobně velkým obloukem vyhýbají. Naše cukrářka Mirka při své práci úročí zkušenosti z dlouholetého působení v Ambiente.
-
-Makronky - nebarvíme - to je základní princip našich makronek. Věříme v chutě surovin a nikoliv barev. Vedle vanilkových a čokoládových vyrábíme také oříškové a pistáciové makronky.
-
-Sachr - náš sachr se liší od toho originálního tím, že je měkčí a vláčnější. Čokoládou ale rozhodně nešetříme.
-
-Cheesecake - lahodná a jemná Philadelphia spolu s jemnou citronovou chutí a křupavým korpusem - to je náš cheesecake.
-
-Ořechový dort - klasickou mouku jsme nahradili zdravější pohankovou. Milovníky ořechů jistě uspokojí.
-
-Black forest - klasický a lahodný. Smetana - čokoláda - višně, a to vše zasypáno hromadou čokoládových hoblin! Víc ke štěstí netřeba.
-
-Tiramisu - geniálně vypilovaná receptura, originální piškoty Savoiardi a kvalitní mascarpone od Galbani.
-
-Makový dort
-
-Profiterol
-
-Naše dorty
-
-Všechny naše dorty vyrábíme ve vlastní řemeslné pekárně v Praze v Dejvicích. Stejně jako u zmrzliny jsme naprosto nekompromisní ve výběru nejlepších surovin, vše se vyrábí ručně a polotovary, práškové směsi a hotové kupované korpusy se naší řemeslné výrobně velkým obloukem vyhýbají. Naše cukrářka Mirka při své práci úročí zkušenosti z dlouholetého působení v Ambiente.
-
-Makronky - nebarvíme - to je základní princip našich makronek. Věříme v chutě surovin a nikoliv barev. Vedle vanilkových a čokoládových vyrábíme také oříškové a pistáciové makronky.
-
-Sachr - náš sachr se liší od toho originálního tím, že je měkčí a vláčnější. Čokoládou ale rozhodně nešetříme.
-
-Cheesecake - lahodná a jemná Philadelphia spolu s jemnou citronovou chutí a křupavým korpusem - to je náš cheesecake.
-
-Ořechový dort - klasickou mouku jsme nahradili zdravější pohankovou. Milovníky ořechů jistě uspokojí.
-
-Black forest - klasický a lahodný. Smetana - čokoláda - višně, a to vše zasypáno hromadou čokoládových hoblin! Víc ke štěstí netřeba.
-
-Tiramisu - geniálně vypilovaná receptura, originální piškoty Savoiardi a kvalitní mascarpone od Galbani.
-
-Makový dort
-
-Profiterol
-
-Naše dorty
-
-Všechny naše dorty vyrábíme ve vlastní řemeslné pekárně v Praze v Dejvicích. Stejně jako u zmrzliny jsme naprosto nekompromisní ve výběru nejlepších surovin, vše se vyrábí ručně a polotovary, práškové směsi a hotové kupované korpusy se naší řemeslné výrobně velkým obloukem vyhýbají. Naše cukrářka Mirka při své práci úročí zkušenosti z dlouholetého působení v Ambiente.
-
-Makronky - nebarvíme - to je základní princip našich makronek. Věříme v chutě surovin a nikoliv barev. Vedle vanilkových a čokoládových vyrábíme také oříškové a pistáciové makronky.
-
-Sachr - náš sachr se liší od toho originálního tím, že je měkčí a vláčnější. Čokoládou ale rozhodně nešetříme.
-
-Cheesecake - lahodná a jemná Philadelphia spolu s jemnou citronovou chutí a křupavým korpusem - to je náš cheesecake.
-
-Ořechový dort - klasickou mouku jsme nahradili zdravější pohankovou. Milovníky ořechů jistě uspokojí.
-
-Black forest - klasický a lahodný. Smetana - čokoláda - višně, a to vše zasypáno hromadou čokoládových hoblin! Víc ke štěstí netřeba.
-
-Tiramisu - geniálně vypilovaná receptura, originální piškoty Savoiardi a kvalitní mascarpone od Galbani.
-
-Makový dort
-
-Profiterol
-
-Naše dorty
-
-Všechny naše dorty vyrábíme ve vlastní řemeslné pekárně v Praze v Dejvicích. Stejně jako u zmrzliny jsme naprosto nekompromisní ve výběru nejlepších surovin, vše se vyrábí ručně a polotovary, práškové směsi a hotové kupované korpusy se naší řemeslné výrobně velkým obloukem vyhýbají. Naše cukrářka Mirka při své práci úročí zkušenosti z dlouholetého působení v Ambiente.
-
-Makronky - nebarvíme - to je základní princip našich makronek. Věříme v chutě surovin a nikoliv barev. Vedle vanilkových a čokoládových vyrábíme také oříškové a pistáciové makronky.
-
-Sachr - náš sachr se liší od toho originálního tím, že je měkčí a vláčnější. Čokoládou ale rozhodně nešetříme.
-
-Cheesecake - lahodná a jemná Philadelphia spolu s jemnou citronovou chutí a křupavým korpusem - to je náš cheesecake.
-
-Ořechový dort - klasickou mouku jsme nahradili zdravější pohankovou. Milovníky ořechů jistě uspokojí.
-
-Black forest - klasický a lahodný. Smetana - čokoláda - višně, a to vše zasypáno hromadou čokoládových hoblin! Víc ke štěstí netřeba.
-
-Tiramisu - geniálně vypilovaná receptura, originální piškoty Savoiardi a kvalitní mascarpone od Galbani.
-
-Makový dort
-
-Profiterol
-
-Naše dorty
-
-Všechny naše dorty vyrábíme ve vlastní řemeslné pekárně v Praze v Dejvicích. Stejně jako u zmrzliny jsme naprosto nekompromisní ve výběru nejlepších surovin, vše se vyrábí ručně a polotovary, práškové směsi a hotové kupované korpusy se naší řemeslné výrobně velkým obloukem vyhýbají. Naše cukrářka Mirka při své práci úročí zkušenosti z dlouholetého působení v Ambiente.
-
-Makronky - nebarvíme - to je základní princip našich makronek. Věříme v chutě surovin a nikoliv barev. Vedle vanilkových a čokoládových vyrábíme také oříškové a pistáciové makronky.
-
-Sachr - náš sachr se liší od toho originálního tím, že je měkčí a vláčnější. Čokoládou ale rozhodně nešetříme.
-
-Cheesecake - lahodná a jemná Philadelphia spolu s jemnou citronovou chutí a křupavým korpusem - to je náš cheesecake.
-
-Ořechový dort - klasickou mouku jsme nahradili zdravější pohankovou. Milovníky ořechů jistě uspokojí.
-
-Black forest - klasický a lahodný. Smetana - čokoláda - višně, a to vše zasypáno hromadou čokoládových hoblin! Víc ke štěstí netřeba.
-
-Tiramisu - geniálně vypilovaná receptura, originální piškoty Savoiardi a kvalitní mascarpone od Galbani.
-
-Makový dort
-
-Profiterol
-
+    <section id="introBanner">
+        <div id="allIntroBanner">
+            <div id="introBannerText">
+                <h1>Vyrábíme nejlepší</h1>
+                <h1>italskou zmrzlinu</h1>
+                <h1>v Praze</h1>
+            </div>
+            <div id="introBannerButton">
+                <a href="#introSecond"><button type="button" class="btn btn-primary">Více o Nás</button></a>
+            </div>
+        </div>
+    </section>
+
+    <section id="introSecond">
+            <div id="textImage">
+                <img src="${iceCream}" alt="Chocolate ice-cream"> 
+            </div>
+            <div id="text">
+                <h1>milujeme zmrzlinu</h1>
+                <p>A tato naše vášeň nás přivedla až do italských končin, kde jsme se několik let učili od nejlepších místních mistrů vyrábět tu nejlahodnější řemeslnou zmrzlinu, která zaručeně uchvátí vaše smysly.
+                Crème de la Crème je kvalita, čistá a dokonalá chuť.
+                A láska. Zamilujte se s námi!</p>
+            </div>
+    </section>
     `
 
     return element
