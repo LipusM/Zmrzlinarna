be570125da22910983fdf18c75baca3726d041f4
M.Lipus <mlip007@gmail.com>
2023-07-17 19:27:48 +0200

    Rozdělaný Form na stránce PRACE

--- src/pages/Prace/components/Form/index.js
+++ src/pages/Prace/components/Form/index.js
@@ -6,5 +6,76 @@ export const Form = () => {
     const element = document.createElement("form")
     element.setAttribute("id", "jobForm")
 
+    element.innerHTML = `
+    <div class="row">
+        <div class="mb-3 col-md-6">
+            <label for="nameSurname" class="form-label">Jméno a příjmení</label>
+            <input type="text" class="form-control" id="nameSurname" placeholder="Zadejte své jméno a příjmení">
+        </div>
+
+        <div class="mb-3 col-md-6">
+            <label for="jobPosition" class="form-label">Zájem o pozici</label>
+            <select class="form-select" id="jobPosition" aria-label="Default select example">
+                <option selected></option>
+                <option value="1">Vedoucí směny</option>
+                <option value="2">Barista</option>
+                <option value="3">Jiná pozice</option>
+            </select>
+        </div>
+
+        <div class="mb-3 col-md-6">
+            <label for="yourEmail" class="form-label">E-mail</label>
+            <input type="email" class="form-control" id="yourEmail" placeholder="Zadejte svůj e-mail">
+        </div>
+
+        <div class="mb-3 col-md-6">
+            <label for="phone" class="form-label">Telefon</label>
+            <input type="text" class="form-control" id="phone" placeholder="Zadejte svůj telefon">
+        </div>
+
+        <div class="mb-3 col-md-6">
+            <label for="cvFile" class="form-label">Životopis</label>
+            <input class="form-control" type="file" id="cvFile">
+        </div>
+
+        <div class="mb-3 col-md-6">
+            <label for="yourImage" class="form-label">Životopis</label>
+            <input class="form-control" type="file" id="yourImage">
+        </div>
+
+        <div class="mb-3 col-md-6">
+            <label for="knowUs" class="form-label">Odkud jste se o nás dozvěděli</label>
+            <select class="form-select" id="knowUs" aria-label="Default select example">
+                <option selected></option>
+                <option value="1">Jobs.cz</option>
+                <option value="2">Prace.cz</option>
+                <option value="3">Facebook</option>
+                <option value="4">Instagram</option>
+                <option value="5">Leták</option>
+                <option value="6">Od známého</option>
+                <option value="7">Jiné</option>
+            </select>
+        </div>
+
+        <div class="mb-3 col-md-6">
+            <label for="fbProfile" class="form-label">Facebook profil</label>
+            <input type="text" class="form-control" id="fbProfile" placeholder="">
+        </div>
+
+        <div class="mb-3 col-md-12">
+            <label for="aboutYou" class="form-label">Něco o tobě</label>
+            <textarea class="form-control" id="aboutYou" rows="6" placeholder="Jaké máš zkušenosti, co chceš dělat, představ se."></textarea>
+        </div>
+
+        <div class="col-md-6">
+            <a href="/ochrana-osobnich-udaju">Informace</a> o ochraně osobních údajů  
+        </div>
+
+        <div class="col-md-6">
+            <button type="submit" class="btn btn-primary">Odeslat</button>
+        </div>
+    </div>
+    `
+
     return element
 }
