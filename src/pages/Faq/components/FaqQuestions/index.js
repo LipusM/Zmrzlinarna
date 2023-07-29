a99df1580ffad5934bc49db6faaaf67baa5b2544
M.Lipus <mlip007@gmail.com>
2023-07-29 11:50:26 +0200

    Rozdělaná sekce s otázkama na stránce Faq.

---
+++ src/pages/Faq/components/FaqQuestions/index.js
@@ -0,0 +1,11 @@
+const c = console.log.bind(document)
+
+import "./style.scss"
+
+export const FaqQuestions = () => {
+
+    const element = document.createElement("section")
+    element.setAttribute("id","faqSection")
+
+    return element
+}
--- src/pages/Faq/index.js
+++ src/pages/Faq/index.js
@@ -3,12 +3,13 @@ const c = console.log.bind(document)
 import "./style.scss"
 
 import { FaqBanner } from "./components/FaqBanner"
+import { FaqQuestions } from "./components/FaqQuestions"
 
 export const Faq = () => {
 
     const element = document.createElement("main")
 
-    element.append(FaqBanner())
+    element.append(FaqBanner(), FaqQuestions())
 
     return element
 }
