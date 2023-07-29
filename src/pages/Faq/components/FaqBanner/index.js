0deae4e6a65dc883a5fa94918ffb5caa2c169ed5
M.Lipus <mlip007@gmail.com>
2023-07-29 11:32:29 +0200

    Banner Faq v podstatě hotov. Možná proběhnou ještě nějaké úpravy.

--- src/pages/Faq/components/FaqBanner/index.js
+++ src/pages/Faq/components/FaqBanner/index.js
@@ -2,11 +2,24 @@ const c = console.log.bind(document)
 
 import "./style.scss"
 
+import arrowDown from "./img/arrowDown.svg"
+
 export const FaqBanner = () => {
 
     const element = document.createElement("section")
+    element.setAttribute("id","faqBanner")
 
-    element.innerHTML = `kl`
+    element.innerHTML = `
+    <div id="faqBannerText">
+        <h1>Chcete se dozvědět více?</h1>
+        <h1>Často kladené dotazy</h1>
+    </div>
+    <div id="faqArrow">
+        <a href="#faqSection">
+            <img src="${arrowDown}" alt="arrow down"> 
+        </a> 
+    </div>
+    `
 
     return element
 }
