fd572268642e2c3444bc38ce2ef0616eb2c0b5ce
M.Lipus <mlip007@gmail.com>
2023-07-31 19:39:54 +0200

    Rozdělané Faq, ovšem nutno vymyslet správné předání komponentě FaqQuestions.

--- src/pages/Faq/components/FaqQuestions/index.js
+++ src/pages/Faq/components/FaqQuestions/index.js
@@ -2,10 +2,17 @@ const c = console.log.bind(document)
 
 import "./style.scss"
 
-export const FaqQuestions = () => {
+export const FaqQuestions = (props) => {
+
+    const { heading, question, answer } = props
+
 
     const element = document.createElement("section")
     element.setAttribute("id","faqSection")
+
+    element.innerHTML = `
+    
+    `
 
     return element
 }
--- src/pages/Faq/index.js
+++ src/pages/Faq/index.js
@@ -2,14 +2,72 @@ const c = console.log.bind(document)
 
 import "./style.scss"
 
-import { FaqBanner } from "./components/FaqBanner"
-import { FaqQuestions } from "./components/FaqQuestions"
+import {
+    FaqBanner
+} from "./components/FaqBanner"
+import {
+    FaqQuestions
+} from "./components/FaqQuestions"
 
 export const Faq = () => {
 
+    const allAquestions = [
+
+        {
+            heading: "Naše zmrzlina",
+            questionsAnswers: [
+                {
+                    question: "Je možné u Vás objednat narozeninový dort?",
+                    answer: "Speciální narozeninové dorty s popisem na přání zatím nevyrábíme, je možné si u nás zakoupit celý dort z nabídky našich dortů. Je ale třeba objednávka alespoň 3 dny předem.",
+                },
+                {
+                    question: "Je možné u Vás objednat narozeninový dort?",
+                    answer: "Speciální narozeninové dorty s popisem na přání zatím nevyrábíme, je možné si u nás zakoupit celý dort z nabídky našich dortů. Je ale třeba objednávka alespoň 3 dny předem.",
+                },
+                {
+                    question: "Jaký je cenový rozdíl mezi veganskými, zdravými a klasickými zmrzlinami?",
+                    answer: "Všechny druhy zmrzliny včetně zdravých a veganských prodáváme maloobchodně vždy za stejnou cenu jen dle velikosti porce. Tak to bylo, je a tak to zůstane.",
+                },
+            ]
+        },
+
+        {
+            heading: "Dárkové poukazy",
+            questionsAnswers: [
+                {
+                    question: "Jaká je platnost dárkového poukazu?",
+                    answer: "Platnost dárkového poukazu je jeden rok.",
+                },
+                {
+                    question: "Kde mohu Váš dárkový poukaz uplatnit?",
+                    answer: "Dárkové poukazy můžete uplatnit ve všech našich provozovnách.",
+                },
+            ]
+        },
+    ]
+
+
+    const sections = allAquestions.map(section => {
+        /* c(section.heading) */
+        c(section.heading)
+        /* c(section.questionsAnswers) */
+
+        const questionAnswer = section.questionsAnswers.map(questionAnswer => {
+            c(questionAnswer.question)
+            c(questionAnswer.answer)
+            
+        })
+        
+
+    })
+    
+
     const element = document.createElement("main")
 
-    element.append(FaqBanner(), FaqQuestions())
+    element.append(FaqBanner(),
+        FaqQuestions({
+
+        }))
 
     return element
 }
