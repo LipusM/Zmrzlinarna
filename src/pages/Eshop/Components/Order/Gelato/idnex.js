b3d32f4f859199873f5b6333b75d64cc57a5f2b1
LipusM <mlipus007@gmail.com>
2023-08-06 18:57:10 +0200

    Úprava struktury u produktu

--- src/pages/Eshop/Components/Order/Gelato/idnex.js
+++ src/pages/Eshop/Components/Order/Gelato/idnex.js
@@ -9,5 +9,13 @@ export const Gelato = (props) => {
     const element = document.createElement("section")
     element.setAttribute("id","gelato")
 
+    element.innerHTML = `
+    <div>
+        <div> <img src="${img}" alt="Gelato!"> </div>
+        <div>${text}</div>
+        <div><button type="button" class="btn btn-primary">Objednat</button></div>
+    </div>
+    `
+
     return element
 }
