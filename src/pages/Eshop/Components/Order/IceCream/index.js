ff420ec344da3cd1a3f6e76322e1022a03e5381a
LipusM <mlipus007@gmail.com>
2023-08-07 19:12:58 +0200

    Úprava komponenty IceCream, aby správně zobrazovala produkty.

--- src/pages/Eshop/Components/Order/IceCream/index.js
+++ src/pages/Eshop/Components/Order/IceCream/index.js
@@ -7,7 +7,26 @@ export const IceCream = (props) => {
     const {img, text} = props
 
     const element = document.createElement("section")
-    element.setAttribute("id","iceCream")
+    element.setAttribute("class","iceCream")
+
+    element.innerHTML = `
+        <div class="myIceCream">
+            <div> <img src="${img}" alt="Gelato!"> </div>
+            <div class="iceCreamText">${text}</div>
+            <div class="orderButton"><button type="button" class="btn btn-primary">Objednat</button></div>
+        </div>
+    `
+
+    const orderButton = element.querySelector("button")
+
+    orderButton.addEventListener("click", () => {
+        
+        if(orderButton.textContent === "Objednat"){
+            orderButton.textContent = "Objednáno"
+        } else {
+            orderButton.textContent = "Objednat"
+        }
+    })
 
     return element
 }
